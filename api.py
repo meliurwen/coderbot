91b55e883400786fb3c573a444d17c09679776c3
Meliurwen <meliurwen@gmail.com>
2018-11-16 15:05:57 +0100

    [WIP] Preparing for the merge with develop. Moved stepbystep functionalities into new APIs and labeled as 'experimental'.

--- api.py
+++ api.py
@@ -10,6 +10,13 @@ import connexion
 import time
 import sqlite3
 
+## Imports for experimental mode
+from pathlib import Path
+import signal
+import os
+import errno
+from program_v2 import Program_v2
+
 bot_config = Config.get()
 bot = CoderBot.get_instance(
     servo=(bot_config.get("move_motor_mode") == "servo"),
@@ -19,6 +26,72 @@ bot = CoderBot.get_instance(
 prog = None
 prog_engine = ProgramEngine.get_instance()
 
+
+## Functions
+# Status File
+def read_statusFile(temp_files_dict):
+    tmp_folder_path = temp_files_dict["tmp_folder_path"]
+    status_fileName = temp_files_dict["status_fileName"]
+
+    # Load the status file
+    with open(tmp_folder_path + status_fileName, "r") as fh:
+        try:
+            data_coderbotStatus = json.loads(fh.read())
+        except Exception as e:
+            print("####### JSON ERROR: "+str(e))
+            print("####### FILE: "+str(fh.read()))
+            print("####### PATH: "+ tmp_folder_path + status_fileName)
+            data_coderbotStatus = None
+        return data_coderbotStatus
+
+
+def write_statusFile(data_coderbotStatus, temp_files_dict):
+    tmp_folder_path = temp_files_dict["tmp_folder_path"]
+    status_fileName = temp_files_dict["status_fileName"]
+
+    # Create the file and if already exists overwites it
+    with open(tmp_folder_path + status_fileName + ".tmp", "w") as fh:
+        fh.write(json.dumps(data_coderbotStatus))
+        os.rename(tmp_folder_path + status_fileName + ".tmp", tmp_folder_path + status_fileName)
+
+
+def write_prog_gen_commands(command, mode, temp_files_dict):
+    tmp_folder_path = temp_files_dict["tmp_folder_path"]
+    status_fileName = temp_files_dict["status_fileName"]
+    prog_gen_commands_fileName = temp_files_dict["prog_gen_commands_fileName"]
+
+    data_prog_gen_commands = {}
+    data_prog_gen_commands["command"] = command
+    data_prog_gen_commands["argument"] = mode
+    # Create the file and if already exists overwites it
+    with open(tmp_folder_path + prog_gen_commands_fileName + ".tmp", "w") as fh:
+        fh.write(json.dumps(data_prog_gen_commands))
+        os.rename(tmp_folder_path + prog_gen_commands_fileName + ".tmp", tmp_folder_path + prog_gen_commands_fileName)
+
+
+# Initialize the status file
+def initialize_coderbotStatus(temp_files_dict):
+    tmp_folder_path = temp_files_dict["tmp_folder_path"]
+    status_fileName = temp_files_dict["status_fileName"]
+    # The try-except has been used in order to avoid race conditions between the evaulation
+    # of the existence of the folder and its creation
+    try:
+        os.makedirs(tmp_folder_path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    # Initial JSON
+    default_status = {"ok":True, "prog_gen":{}, "prog_handler":{"mode": "stop"}}
+    write_statusFile(default_status, temp_files_dict)
+
+
+# Initialize the status file
+temp_files_dict = {"tmp_folder_path": "tmp/", "status_fileName": "coderbotStatus_temp.json", "prog_gen_commands_fileName": "coderbotProg_gen_commands_temp.json"}
+initialize_coderbotStatus(temp_files_dict)
+
+
+
+## API
 
 def stop():
     bot.stop()
@@ -87,3 +160,55 @@ def restoreSettings():
         Config.write(json.loads(f.read()))
     bot_config = Config.get()
     return "ok"
+
+def exec_experimental(data):
+    code = data["code"]
+    mode = data["mode"]
+
+
+    # Load the status file
+    data_coderbotStatus = read_statusFile(temp_files_dict)
+
+    # if read_statusFile(temp_files_dict) returns None type, a reboot is needed
+    if data_coderbotStatus is None:
+        return json.dumps({"ok":False, "error_code":"coderbotStatusJsonError"}), 500
+
+
+    if data_coderbotStatus["prog_handler"]["mode"] != "stop":
+        prog_gen_pid = int(data_coderbotStatus["prog_gen"]["pid"])
+        try:
+            os.kill(prog_gen_pid, 0)
+        except OSError:
+            prog_gen_is_up = False
+        else:
+            prog_gen_is_up = True
+        if not prog_gen_is_up:
+            data_coderbotStatus["prog_gen"] = {}
+            data_coderbotStatus["prog_handler"]["mode"] = "stop"
+            write_statusFile(data_coderbotStatus, temp_files_dict)
+
+    if data_coderbotStatus["prog_handler"]["mode"] == "stop":
+        data_coderbotStatus["prog_handler"]["mode"] = mode
+        write_statusFile(data_coderbotStatus, temp_files_dict)
+        evaulation = Program_v2.run(code, mode, temp_files_dict)
+    else: # data_coderbotStatus["prog_handler"]["mode"] == "stepByStep" or "fullExec"
+        if mode == "stop":
+            signal_to_program = signal.SIGTERM
+        else: #else mode == "fullExec" or mode == stepByStep"
+            write_prog_gen_commands("change_mode", mode, temp_files_dict)
+            signal_to_program = signal.SIGUSR1
+
+        try:
+            os.kill(prog_gen_pid, signal_to_program)
+        except Exception as err:
+            # The process in reality was already terminated, probably because of some crash or the previous program unexpectedly terminated after the "data_coderbotStatus" dict
+            # has been retrieved from the file (this last case is really unlikely to happen).
+            print("######### error: "+str(err))
+
+        evaulation = {"ok":True,"description":""}
+
+    # Returns a positive or error response
+    if evaulation["ok"]:
+        return json.dumps(evaulation), 200
+    else:
+        return json.dumps(evaulation), evaulation["error_code"]
---
+++ program_v2.py
@@ -0,0 +1,89 @@
+import subprocess
+ 
+class Program_v2:
+
+    def run(code, mode, temp_files_dict):
+
+        tmp_folder_path = temp_files_dict["tmp_folder_path"]
+        status_fileName = temp_files_dict["status_fileName"]
+        prog_gen_commands_fileName = temp_files_dict["prog_gen_commands_fileName"]
+
+        try:
+
+            if mode == "fullExec":
+                is_execFull = "is_execFull = True\n"
+            else: # mode == stepByStep
+                is_execFull = "is_execFull = False\n"
+
+            headerFile = is_execFull + '\n\
+\n\
+import json\n\
+from os import getpid, rename\n\
+import sys\n\
+import signal\n\
+with open("' + tmp_folder_path + status_fileName + '", "r") as fh:\n\
+ data_coderbotStatus = json.loads(fh.read())\n\
+\n\
+def saveStatus():\n\
+ with open("' + tmp_folder_path + status_fileName + '.tmp", "w") as fh:\n\
+  fh.write(json.dumps(data_coderbotStatus))\n\
+  rename("' + tmp_folder_path + status_fileName + '.tmp", "' + tmp_folder_path + status_fileName + '")\n\
+\n\
+data_coderbotStatus["prog_gen"]["currentBlockId"] = None\n\
+data_coderbotStatus["prog_gen"]["status"] = "loading"\n\
+data_coderbotStatus["prog_gen"]["pid"] = getpid()\n\
+saveStatus()\n\
+print("####### "+str(data_coderbotStatus["prog_gen"]["pid"]))\n\
+print("###### LAUNCHED")\n\
+print("###### IMPORTING prog_gen_commands.py MODULE...")\n\
+\n\
+from prog_gen_commands import Commands\n\
+\n\
+print("###### MODULE IMPORTED")\n\
+data_coderbotStatus["prog_gen"]["status"] = "running"\n\
+saveStatus()\n\
+\n\
+def do_command(sig, stack):\n\
+ global is_execFull\n\
+ with open("' + tmp_folder_path + prog_gen_commands_fileName + '", "r") as fh:\n\
+  data_prog_gen_commands = json.loads(fh.read())\n\
+\n\
+ if data_prog_gen_commands["command"] == "change_mode":\n\
+  if data_prog_gen_commands["argument"] == "fullExec":\n\
+   is_execFull = True\n\
+   data_coderbotStatus["prog_gen"]["status"] = "running"\n\
+  elif data_prog_gen_commands["argument"] == "stepByStep":\n\
+   is_execFull = False\n\
+   data_coderbotStatus["prog_gen"]["status"] = "running"\n\
+  else:\n\
+   pass # Ignore if the argument is unknown\n\
+ else:\n\
+  pass # Ignore if the command is unknown\n\
+ saveStatus()\n\
+def do_terminate(sig, stack):\n\
+ data_coderbotStatus["prog_gen"] = {}\n\
+ data_coderbotStatus["prog_handler"]["mode"] = "stop"\n\
+ saveStatus()\n\
+ print("######### PROGRAM TERMINATED")\n\
+ sys.exit(0)\n\
+signal.signal(signal.SIGUSR1, do_command)\n\
+signal.signal(signal.SIGTERM, do_terminate)\n\
+\n'
+
+            footerFile = 'data_coderbotStatus["prog_gen"] = {}\ndata_coderbotStatus["prog_handler"]["mode"] = "stop"\nsaveStatus()\nprint("######### PROGRAM TERMINATED")'
+
+            code = headerFile + code + footerFile
+
+            print("######## PREPARING THE FILE...")
+            with open("_coderbot_generated_program.tmp.py", "w") as fh:
+                fh.write(code)
+            print("######## THE FILE IS READY")
+            print("######## LAUNCHING...")
+            subprocess.Popen(["python3", "_coderbot_generated_program.tmp.py"])
+
+            return {"ok":True,"description":""}
+        except Exception as e:
+            return {"ok":False,"error_code":500,"description":"ProblemOnLauchingTheGeneratedProgram"}
+
+
+
